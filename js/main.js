7b3a24426085a8266508c68b5cd0f189e93b04de
Michelle Gienow <michellegienow@gmail.com>
2016-07-19 20:26:13 -0400

    version with weird flex box height disparity in buy section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*var about = document.querySelector("a[href='#about']");
+var target = document.getElementById('about');
+
+
+aboutLink.addEventListener('click', function(event) {
+  if (window.scrollTo) {
+    event.preventDefault();
+    window.scrollTo({"behavior": "smooth", "top": target.offsetTop});
+  }
+});*/
+
+var slides = document.querySelectorAll('#quotes .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 6000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
